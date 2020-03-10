2897c01c58e54222770f9d0134e3513bb9337a04
Nuwan <nuwan@aasait.com>
2020-03-10 23:58:19 +0530

    create Persons Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
-    </div>
-  );
+import Person from './Person/Person';
+class App extends Component{
+  state = {
+    persons:[
+      {name:'Max',age:28},
+      {name:'Manu',age:30},
+      {name:'Jhon',age:25}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi, welcome to React</h1>
+        <p>This is reallyworking</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
